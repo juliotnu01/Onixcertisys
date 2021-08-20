2484745db0d8884f37c69660957c2c147a0948b3
Juliot <developer4@otp.com.co>
2021-08-20 03:33:05 -0500

    daasd

--- app/Http/Controllers/v1/CalibracionController.php
+++ app/Http/Controllers/v1/CalibracionController.php
@@ -92,6 +92,7 @@ class CalibracionController extends Controller
             $url = Storage::disk('s3')->url('Excel-certificados/'.$request->file('certificado')->getClientOriginalName());
             $dataPdf = ["id_partida" => $data['id'], "doc_path" => $url];
             $d = collect($dataPdf);
+            dd($d->all());
             $r =  Http::post(env('API_HANDLE_FILE_EXCEL_DOC')."/api/Pdf/Json", $d->all());
             return DB::transaction(function () use ($request, $calibracion) {
                 $calibracion->find($request['id_calibracion'])->update([
