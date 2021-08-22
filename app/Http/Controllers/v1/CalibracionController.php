4a70f1af9c9084694efe7c2e9532413141b79331
Juliot <developer4@otp.com.co>
2021-08-21 21:13:37 -0500

    asdasdad

--- app/Http/Controllers/v1/CalibracionController.php
+++ app/Http/Controllers/v1/CalibracionController.php
@@ -87,7 +87,7 @@ class CalibracionController extends Controller
             $id = $data['id'];
             Storage::disk('s3')->putFileAs(Carbon::now()->format('Y-m-d')."/".$data['has_recibo']->has_cotizaicon->has_cliente->datos_fisicos_requeremientos_facturacion_razon_social."/partida-".$id."/partida-calibracion/",$request->file('certificado'),$request->file("certificado")->getClientOriginalName());
             $url = Storage::disk('s3')->url(Carbon::now()->format('Y-m-d')."/".$data['has_recibo']->has_cotizaicon->has_cliente->datos_fisicos_requeremientos_facturacion_razon_social."/partida-".$id."/partida-calibracion/".$request->file("certificado")->getClientOriginalName());
-            $dataPdf = ["id_partida" => $data['id'], "doc_path" => str_replace('%', ' ', $url)];
+            $dataPdf = ["id_partida" => $data['id'], "doc_path" =>$url];
             $d = collect($dataPdf);
             $r =  Http::post(env('API_HANDLE_FILE_EXCEL_DOC')."/api/Pdf/Json", $d->all());
             return DB::transaction(function () use ($request, $calibracion) {
