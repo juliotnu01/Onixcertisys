d03b5d81a4ede600c3cccd805b88d9b155675c16
Ubuntu <ubuntu@ip-172-31-35-205.us-east-2.compute.internal>
2021-09-06 05:50:08 +0000

    commit temporal

--- app/Http/Controllers/v1/EmpleadoController.php
+++ app/Http/Controllers/v1/EmpleadoController.php
@@ -185,8 +185,12 @@ class EmpleadoController extends Controller
                     $calidad->save();
                 }
 
-            }, 5);
-            $r =  Http::post(env('API_HANDLE_FILE_EXCEL_DOC')."/api/Asignacion/Json", $request->all());
+	    }, 5);
+	    $collection  =  collect($request);
+	    
+	    $filtered = $collection->except(['model.has_calidad']);
+	   
+            $r =  Http::post(env('API_HANDLE_FILE_EXCEL_DOC')."/api/Asignacion/Json", $filtered->all());
             dd($r);
         } catch (Exception $e) {
             throw new Exception($e, 1);
